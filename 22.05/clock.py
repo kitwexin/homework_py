7b28ca82170862cb3b01fc82682edc523edbc88b
Aleksey Demin <kitwexin@gmail.com>
2020-05-22 21:15:11 +0300

    color

--- 22.05/clock.py
+++ 22.05/clock.py
@@ -6,6 +6,7 @@ def getnum(matrix):
     txt = matrix.replace('1','\u2593')
     txt = txt.replace('0',' ')
     return [ x for x in txt.split('\n') if x ]
+
 n0 = getnum("""
 11111
 10001
@@ -83,16 +84,34 @@ nd = getnum(r"""
 00100
 00000
 """)
+
 trans = {
     '0':n0,'1':n1,'2':n2,'3':n3,'4':n4,'5':n5,'6':n6,'7':n7,'8':n8,'9':n9,':':nd
 }
+
+counter = 0
+
 while True:
     txt = ""
+    counter += 1
     for x in range(5):
         for y in datetime.datetime.now().strftime('%X'):
             txt += trans[y][x]
             txt += " "
         txt += '\n'
-    print(txt)
-    time.sleep(1)
-    os.system('cls||clear')
+    if counter%1 == 0 and counter%2 != 0 and counter%3 != 0:
+        print("\033[31m{}" .format(txt))
+        time.sleep(1)
+        os.system('cls||clear')
+    elif counter%2 == 0 and counter%3 != 0:
+        print("\033[33m{}" .format(txt))
+        time.sleep(1)
+        os.system('cls||clear')
+    elif counter%3 == 0 and counter%2 != 0:
+        print("\033[34m{}" .format(txt))
+        time.sleep(1)
+        os.system('cls||clear')
+    else:
+        print("\033[32m{}" .format(txt))
+        time.sleep(1)
+        os.system('cls||clear')
